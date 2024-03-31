0aae4a86675400d9dad63c4bfd97c5744c37fddc
Sahil Ranjan <ranjan.sahil.2001@gmail.com>
2024-03-31 23:37:56 +0530

    github data added

--- index.js
+++ index.js
@@ -5,6 +5,41 @@ const app = express()
 
 const port = 4000
 
+const githubData = {
+    "login": "isahil18",
+    "id": 111517894,
+    "node_id": "U_kgDOBqWgxg",
+    "avatar_url": "https://avatars.githubusercontent.com/u/111517894?v=4",
+    "gravatar_id": "",
+    "url": "https://api.github.com/users/isahil18",
+    "html_url": "https://github.com/isahil18",
+    "followers_url": "https://api.github.com/users/isahil18/followers",
+    "following_url": "https://api.github.com/users/isahil18/following{/other_user}",
+    "gists_url": "https://api.github.com/users/isahil18/gists{/gist_id}",
+    "starred_url": "https://api.github.com/users/isahil18/starred{/owner}{/repo}",
+    "subscriptions_url": "https://api.github.com/users/isahil18/subscriptions",
+    "organizations_url": "https://api.github.com/users/isahil18/orgs",
+    "repos_url": "https://api.github.com/users/isahil18/repos",
+    "events_url": "https://api.github.com/users/isahil18/events{/privacy}",
+    "received_events_url": "https://api.github.com/users/isahil18/received_events",
+    "type": "User",
+    "site_admin": false,
+    "name": "Sahil Ranjan",
+    "company": null,
+    "blog": "",
+    "location": "Deoghar, Jharkhand, India",
+    "email": null,
+    "hireable": true,
+    "bio": null,
+    "twitter_username": "isahil_18",
+    "public_repos": 4,
+    "public_gists": 0,
+    "followers": 0,
+    "following": 1,
+    "created_at": "2022-08-18T14:23:54Z",
+    "updated_at": "2024-03-31T17:51:44Z"
+  } 
+
 app.get('/', (req, res) => {
   res.send('Hello World!')
 })
@@ -19,6 +54,10 @@ app.get('/login', (req, res) => {
 
 app.get('/youtube', (req, res) => {
     res.send('<h2>silly mates</h2>')
+})
+
+app.get("/github", (req, res) => {
+    res.json(githubData)
 })
 
 app.listen(process.env.PORT, () => {
